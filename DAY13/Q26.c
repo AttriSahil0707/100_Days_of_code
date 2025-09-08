dfd8545523c2a96495e3c9647934d6364ac251cb
AttriSahil <sahilattrisa2007@gmail.com>
2025-09-08 22:55:31 +0530

    Create Q26.c

---
+++ DAY13/Q26.c
@@ -0,0 +1,21 @@
+/*
+Q26 (Loops without Arrays/Strings)
+Write a program to print numbers from 1 to n.
+*/
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    printf("Numbers from 1 to %d are:\n", n);
+
+    // Using for loop
+    for (i = 1; i <= n; i++) {
+        printf("%d ", i);
+    }
+
+    return 0;
+}
