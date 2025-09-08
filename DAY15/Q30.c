108b58aa2c77dbaccecfff0b331ccfaea07ca90a
AttriSahil <sahilattrisa2007@gmail.com>
2025-09-08 23:01:00 +0530

    Create Q30.c

---
+++ DAY15/Q30.c
@@ -0,0 +1,22 @@
+/*Q30 (Loops without Arrays/Strings)
+Write a program to reverse a given number.
+
+*/
+#include <stdio.h>
+
+int main() {
+    int num, rev = 0, digit;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while (num > 0) {
+        digit = num % 10;           // take last digit
+        rev = rev * 10 + digit;     // build reversed number
+        num = num / 10;             // remove last digit
+    }
+
+    printf("Reversed number = %d\n", rev);
+
+    return 0;
+}
