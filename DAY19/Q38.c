4801f69503b2c22d870024f7f26da48bcc4ac8c5
AttriSahil <sahilattrisa2007@gmail.com>
2025-09-14 23:12:40 +0530

    Create Q38.c

---
+++ DAY19/Q38.c
@@ -0,0 +1,25 @@
+/*Q38 (Loops without Arrays/Strings)
+Write a program to find the sum of digits of a number.
+
+*/
+
+#include <stdio.h>
+int main(){
+    int num,digit,sum=0;
+    printf("Enter a number: ");
+    scanf("%d",&num);
+    if (num < 0) {
+        num = -num; 
+    }
+
+ 
+    while (num > 0) {
+        digit = num % 10;     
+        sum += digit;         
+        num /= 10;            
+    }
+
+    printf("Sum of digits: %d\n", sum);
+
+    return 0;
+}
