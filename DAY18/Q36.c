298d2c33142a64c170c16b94a4cc4b2df5585246
AttriSahil <sahilattrisa2007@gmail.com>
2025-09-14 23:11:40 +0530

    Create Q36.c

---
+++ DAY18/Q36.c
@@ -0,0 +1,22 @@
+/*Q36 (Loops without Arrays/Strings)
+Write a program to find the HCF (GCD) of two numbers.
+
+*/
+
+#include <stdio.h>
+int main(){
+    int a,b,i,hcf;
+    printf("Enter two numbers: ");
+    scanf("%d %d",&a,&b);
+    int min= ( a<b ) ? a:b;
+    for (i = 1; i <= min; i++) {
+        if (a % i == 0 && b % i == 0) {
+            hcf = i;
+        }
+    }
+    printf("%d", hcf);
+
+    return 0;
+
+    
+}
