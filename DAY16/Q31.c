88a068d878b17fb0de08e50f0d52f9959f25bed8
AttriSahil <sahilattrisa2007@gmail.com>
2025-09-14 23:08:25 +0530

    Create Q31.c

---
+++ DAY16/Q31.c
@@ -0,0 +1,24 @@
+/*
+Q31 (Loops without Arrays/Strings)
+Write a program to take a number as input and print its equivalent binary representation.
+
+*/
+
+#include <stdio.h>
+int main(){
+    int num,rem,place=1,binary=0;
+
+    printf("Enter a number:");
+    scanf("%d",&num);
+    if (num ==0){
+        printf("Binary = 0\n");}
+    while (num>0)
+    {
+        rem = num %2;
+        binary= binary + rem*place;
+        place=place*10;
+        num = num/2;
+    }
+    printf("Binary =%d\n",binary);
+    return 0;
+}
