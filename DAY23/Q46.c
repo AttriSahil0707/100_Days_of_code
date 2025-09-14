64b71e71caa82885e3ae6d0e95a4ece3815cf487
AttriSahil <sahilattrisa2007@gmail.com>
2025-09-14 23:36:41 +0530

    Create Q46.c

---
+++ DAY23/Q46.c
@@ -0,0 +1,17 @@
+/*Q46 (Nested Loops without Arrays/Strings)
+Write a program to print the following pattern:
+*****
+*****
+*****
+*****
+*****   */
+
+#include <stdio.h>
+int main(){
+    int i;
+    for ( i = 0; i < 5; i++)
+    {
+        printf("*****\n");
+    }
+    return 0;
+}
