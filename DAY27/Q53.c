1d97362d8128caea50ae28219c06fd0548be871c
AttriSahil <sahilattrisa2007@gmail.com>
2025-09-20 00:01:41 +0530

    Update Q53.c

--- DAY27/Q53.c
+++ DAY27/Q53.c
@@ -15,8 +15,6 @@ Write a program to print the following pattern:
 
 int main() {
     int i, j, stars;
-
-    // Upper part of the pattern (1 to 5 lines)
     for (i = 1; i <= 5; i++) {
         stars = 2 * i - 1;  // 1, 3, 5, 7, 9
         for (j = 1; j <= stars; j++) {
@@ -25,7 +23,6 @@ int main() {
         printf("\n");
     }
 
-    // Lower part of the pattern (4 to 1 lines)
     for (i = 4; i >= 1; i--) {
         stars = 2 * i - 1;  // 7, 5, 3, 1
         for (j = 1; j <= stars; j++) {
