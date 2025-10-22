7e20a812190896461b9b20255a13748808f3dd70
AttriSahil <sahilattrisa2007@gmail.com>
2025-10-22 21:48:20 +0530

    Add program to find ceil index in sorted array
    Implement a program to find the ceil index of a given value in a sorted array.

---
+++ DAY52/Q102.c
@@ -0,0 +1,34 @@
+/*
+Q102 (Logic Enhancers)
+Write a Program to take a sorted array arr[] and an integer x as input, find the index (0-based) of the smallest element in arr[] that is greater than or equal to x and print it. 
+This element is called the ceil of x. If such an element does not exist, print -1. Note: In case of multiple occurrences of ceil of x, return the index of the first occurrence.
+*/
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, x, i, index = -1;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter sorted array elements: ");
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Enter value of x: ");
+    scanf("%d", &x);
+
+    for(i = 0; i < n; i++) {
+        if(arr[i] >= x) {
+            index = i;
+            break;
+        }
+    }
+
+    if(index == -1)
+        printf("-1");
+    else
+        printf("Ceil index: %d", index);
+
+    return 0;
+}
