9c31ba1749d84078ecde9c53af3c5a29630740ae
AttriSahil <sahilattrisa2007@gmail.com>
2026-03-30 21:26:42 +0530

    Fix typo in input prompt for positive integer
    Corrected a typo in the prompt for user input.

--- DAY22/Q43.c
+++ DAY22/Q43.c
@@ -8,7 +8,7 @@ Write a program to check if a number is a strong number.
 int main() {
     int num, originalNum, digit, sum = 0;
 
-    printf("Enter a positive integer: ");
+    printf("Enter a positisfdfdfve integer: ");
     scanf("%d", &num);
 
     if (num < 0) {
