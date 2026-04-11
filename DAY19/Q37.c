6e8a8fa9aa2639817ce9df7ed54223e21cbeff8e
AttriSahil <sahilattrisa2007@gmail.com>
2026-04-11 22:03:32 +0530

    Update Q37.c

--- DAY19/Q37.c
+++ DAY19/Q37.c
@@ -6,7 +6,7 @@ Write a program to find the LCM of two numbers.
 #include <stdio.h>
 int main(){
     int a,b,i,hcf;
-    printf("Enter two numbers: ");
+    printf("Enter two numbedsdsdrs: ");
     scanf("%d %d",&a,&b);
     int min= ( a<b ) ? a:b;
     for (i = 1; i <= min; i++) {
